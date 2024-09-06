e423b6506cf2f1aea4c6578bb30cc995ca17fe6d
OlehandroO <velichkosasa@gmail.com>
2024-09-06 21:28:29 +0400

    HTML file

---
+++ Python/test.py
@@ -0,0 +1,33 @@
+a=5
+b=2
+
+
+# if (a == b):
+#     print("a отличается от b")
+# elif ((a==b) or (a>b)):
+#     print ("a не отличается от b")
+
+
+# >, <, <= , >=, ==, =!, or, and, not 
+
+
+
+#Циклы
+#while for
+
+n=10
+i=1
+sum=0
+a=0
+
+# for i in "pizdec":
+#     print(i*2, end="")
+
+
+x=10
+y=5
+
+z = x//y
+print(z)
+z = x%y
+print(z)
